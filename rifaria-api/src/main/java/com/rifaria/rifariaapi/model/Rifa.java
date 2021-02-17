c9ce4f8140afe97985f7fe0f91e51fd2e774ecfb
Pedro Ferreira <pedro.ferreira.da.silva@sinch.com>
2021-02-16 22:20:08 -0300

    Removing unused imports

--- rifaria-api/src/main/java/com/rifaria/rifariaapi/model/Rifa.java
+++ rifaria-api/src/main/java/com/rifaria/rifariaapi/model/Rifa.java
@@ -5,19 +5,10 @@ import java.io.Serializable;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Table(name = "rifa")
 @Entity
 public class Rifa implements Serializable {
